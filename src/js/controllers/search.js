38d3d81112c6ebf9da44943454b9ee5f43e14487
gagarin55 <gagarin55@protonmail.com>
2016-06-12 11:34:36 +0300

    1W for address for better UX

--- src/js/controllers/search.js
+++ src/js/controllers/search.js
@@ -1,42 +1,43 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	function SearchCtrl($scope, $state, $http, apiMethods) {
+    function SearchCtrl($scope, $state, $http, apiMethods) {
 
-		$scope.search = search;
+        $scope.search = search;
 
-		function search() {
-			var q = $scope.searchQuery;
-			if (!q)
-				return;
-			// check address
-			$http.get(apiMethods.address.validate(q))
-					.success(function (data) {
-						if (data.valid)
-							$state.go('address-details', {address: q});
-						else {
-							// check block
-							$http.get(apiMethods.blocks.bySignature(q))
-									.success(function (data) {
-										if (!data.error) {
-											$state.go('block-details-sig', {signature: q})
-										} else {
-											// check tx
-											$http.get(apiMethods.transactions.info(q))
-													.success(function(data){
-														if (!data.error)
-															$state.go('tx-details', {signature:q});
-											})
-										}
-									});
-						}
-					})
-					.error(function (data) {
-					});
+        function search() {
+            var q = $scope.searchQuery;
+            if (!q)
+                return;
+            // check address
+            var addr = q.substring(2);
+            $http.get(apiMethods.address.validate(addr))
+                    .success(function (data) {
+                        if (data.valid)
+                            $state.go('address-details', {address: addr});
+                        else {
+                            // check block
+                            $http.get(apiMethods.blocks.bySignature(q))
+                                    .success(function (data) {
+                                        if (!data.error) {
+                                            $state.go('block-details-sig', {signature: q})
+                                        } else {
+                                            // check tx
+                                            $http.get(apiMethods.transactions.info(q))
+                                                    .success(function (data) {
+                                                        if (!data.error)
+                                                            $state.go('tx-details', {signature: q});
+                                                    })
+                                        }
+                                    });
+                        }
+                    })
+                    .error(function (data) {
+                    });
 
-		}
-	}
+        }
+    }
 
-	angular.module('web').controller('SearchCtrl', SearchCtrl);
+    angular.module('web').controller('SearchCtrl', SearchCtrl);
 })();
 
