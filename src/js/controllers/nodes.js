41db5527d41a948e68239b5afabb492cabbf4c2a
gagarin55 <gagarin55@protonmail.com>
2016-06-13 11:46:40 +0300

    one more community node in list

--- src/js/controllers/nodes.js
+++ src/js/controllers/nodes.js
@@ -1,8 +1,8 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	function NodesCtrl($http, apiProvider) {
-		var ctrl = this;
+    function NodesCtrl($http, apiProvider) {
+        var ctrl = this;
 
 //		ctrl.nodes = [
 //			{ url: 'http://52.36.177.184:6869' },
@@ -13,52 +13,50 @@
 //			{ url: 'http://52.63.116.99:6869' }
 //		];
 
-		ctrl.nodes = [
-			{ url: 'http://52.30.47.67:6869', maintainer:'Waves' },
-			{ url: 'http://52.28.66.217:6869', maintainer:'Waves' },
-			{ url: 'http://52.77.111.219:6869', maintainer:'Waves' },
-			{ url: 'http://79.140.41.223:6869', maintainer:'Community'}
-		];
+        ctrl.nodes = [
+            {url: 'http://52.30.47.67:6869', maintainer: 'Waves'},
+            {url: 'http://52.28.66.217:6869', maintainer: 'Waves'},
+            {url: 'http://52.77.111.219:6869', maintainer: 'Waves'},
+            {url: 'http://79.140.41.223:6869', maintainer: 'Community'},
+            {url: 'http://88.76.147.242:6869', maintainer: 'Community'}
+        ];
 
-		activate();
+        activate();
 
-		function activate() {
-			ctrl.nodes.forEach(function (node) {
-				
-				$http.get(apiProvider(node.url).version)
-						.success(function (data) {
-							node.version = data.version;
-						}).error(function () {
-							node.version = "error";
-						});
-						
-				$http.get(apiProvider(node.url).blocks.height)
-						.success(function (data) {
-							node.height = data.height;
-						}).error(function () {
-							node.height = "error";
-						});
-						
-														
-				$http.get(apiProvider(node.url).consensus.basetarget)
-						.success(function (data) {
-							node.baseTarget = data.baseTarget;
-						}).error(function () {
-							node.baseTarget = "error";
-						});
-				$http.get(apiProvider(node.url).debug.info)
-						.success(function (data) {
-							node.debugStateHeight = data.stateHeight;
-							node.debugStateHash = data.stateHash;
-						}).error(function () {
-							node.debugStateHeight = "error";
-							node.debugStateHash = "-";
-						});
-					});
+        function activate() {
+            ctrl.nodes.forEach(function (node) {
 
-						
-		}
-	}
+                $http.get(apiProvider(node.url).version)
+                        .success(function (data) {
+                            node.version = data.version;
+                        }).error(function () {
+                    node.version = "error";
+                });
 
-	angular.module('web').controller('NodesCtrl', NodesCtrl);
+                $http.get(apiProvider(node.url).blocks.height)
+                        .success(function (data) {
+                            node.height = data.height;
+                        }).error(function () {
+                    node.height = "error";
+                });
+
+                $http.get(apiProvider(node.url).consensus.basetarget)
+                        .success(function (data) {
+                            node.baseTarget = data.baseTarget;
+                        }).error(function () {
+                    node.baseTarget = "error";
+                });
+                $http.get(apiProvider(node.url).debug.info)
+                        .success(function (data) {
+                            node.debugStateHeight = data.stateHeight;
+                            node.debugStateHash = data.stateHash;
+                        }).error(function () {
+                    node.debugStateHeight = "error";
+                    node.debugStateHash = "-";
+                });
+            });
+        }
+    }
+
+    angular.module('web').controller('NodesCtrl', NodesCtrl);
 })();
