b41fdf75622d97739201c8f45167d6082f9186ae
gagarin55 <gagarin55@protonmail.com>
2016-06-17 16:41:04 +0300

    new testnet node

--- src/js/controllers/nodes.js
+++ src/js/controllers/nodes.js
@@ -17,6 +17,7 @@
             {url: 'http://52.30.47.67:6869', maintainer: 'Waves'},
             {url: 'http://52.28.66.217:6869', maintainer: 'Waves'},
             {url: 'http://52.77.111.219:6869', maintainer: 'Waves'},
+            {url: 'http://52.51.92.182:6869', maintainer: 'Waves'},
             {url: 'http://79.140.41.223:6869', maintainer: 'Community'},
             {url: 'http://88.76.147.242:6869', maintainer: 'Community'}
         ];
