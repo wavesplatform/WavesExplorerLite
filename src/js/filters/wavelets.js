db54bb6f7be698cf4b4b69095bc66e6794b4ced7
gagarin55 <gagarin55@protonmail.com>
2016-07-28 14:53:19 +0300

    waveslets filter fixed to show amount in correct format

--- src/js/filters/wavelets.js
+++ src/js/filters/wavelets.js
@@ -1,11 +1,23 @@
-(function () {
+(function() {
     'use strict';
 
-    angular.module('web').
-            filter('wavelets', function () {
-                return function (input) {
-                    var waves = input / 100000000;
-                    return Number(waves.toFixed(8));
-                };
+    angular.module('web').filter('wavelets', function() {
+        return function(input) {
+            if (input == null) return null;
+            var waves = input / 100000000;
+            var str = waves.toLocaleString("en-US", {
+                minimumFractionDigits: 8
             });
+            var parts = str.split(".")
+            if (parseInt(parts[1]) == 0) parts[1] = "0"
+            else parts[1] = trimZeros(parts[1])
+            return parts[0] + "." + parts[1]
+        };
+
+        function trimZeros(str) {
+            if (str.length == 0 || str[str.length - 1] != "0")
+                return str;
+            return trimZeros(str.substring(0, str.length - 1))
+        }
+    });
 })();
