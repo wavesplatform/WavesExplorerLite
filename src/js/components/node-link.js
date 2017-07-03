db4df3f218112e4dce9368a857c9ee1201ed8296
beregovoy68 <s.a.ishchenko@gmail.com>
2017-07-04 00:22:29 +1000

    FIxed template for node-link component. Link to a node showed up twice.

--- src/js/components/node-link.js
+++ src/js/components/node-link.js
@@ -16,8 +16,8 @@
                 ref: '<'
             },
             template: '<ng-switch on="$ctrl.ref.showAsLink">' +
-                '<a ng-switch-when="true" ng-href="$ctrl.ref.url" target="_blank">{{::$ctrl.ref.url}}</a>' +
-                '<ng-switch-default>{{::$ctrl.ref.url}}</ng-switch-default>' +
+                '<a ng-switch-when="true" ng-href="{{::$ctrl.ref.url}}" target="_blank">{{::$ctrl.ref.url}}</a>' +
+                '<div ng-switch-default>{{::$ctrl.ref.url}}</div>' +
             '</ng-switch>'
         });
 })();
