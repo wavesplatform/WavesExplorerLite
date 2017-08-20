ec5249561980df5734ff58ab12eb4286b1dd0139
beregovoy68 <s.a.ishchenko@gmail.com>
2017-08-20 19:07:34 +1000

    Added poor man's support of exchange transactions

--- src/js/config.js
+++ src/js/config.js
@@ -1,28 +1,21 @@
-(function () {
+(function() {
     'use strict';
 
     angular.module('web').constant('appConfig', {
-        apiDomain: 'http://52.30.47.67:6869',
-        title: 'Waves Testnet Explorer',
-        blockchainName: 'Testnet',
-        nodes: [
-            {url: 'http://52.30.47.67:6869', maintainer: 'Waves', showAsLink: true},
-            {url: 'http://52.28.66.217:6869', maintainer: 'Waves', showAsLink: true},
-            {url: 'http://52.77.111.219:6869', maintainer: 'Waves', showAsLink: true},
-            {url: 'http://52.51.92.182:6869', maintainer: 'Waves', showAsLink: true}
-        ],
-        faucet: {
-            url: 'http://52.30.47.67:9000',
-            captchaKey: '6Ld58SkTAAAAAHJZg2bR4LWFGRrMlwXZrx2IhO2O'
-        },
+        apiDomain: 'https://nodes.wavesnodes.com',
+        blockchainName: 'Mainnet',
+        title: 'Waves Explorer',
+        nodes: [{
+            url: 'https://nodes.wavesnodes.com',
+            maintainer: 'Waves'
+        }],
         peerExplorer: {
-            url: 'https://wavesexplorer.com',
-            title: 'MAINNET Explorer'
+            url: 'https://testnet.wavesexplorer.com',
+            title: 'TESTNET Explorer'
         },
         wallet: {
-            url: 'https://testnet.waveswallet.io',
+            url: 'https://waveswallet.io',
             title: 'Wallet'
         }
     });
-
 })();
