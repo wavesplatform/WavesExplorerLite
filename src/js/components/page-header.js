75fdf2f6a8765d0159717751169ddde8e9f94415
beregovoy68 <s.a.ishchenko@gmail.com>
2017-11-20 01:17:57 +1100

    Added new component for page headers. Migrated address page.

---
+++ src/js/components/page-header.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    function WavesPageHeaderController() {
+        var ctrl = this;
+
+        ctrl.$onInit = function () {
+        };
+    }
+
+    angular
+        .module('web')
+        .component('wavesPageHeader', {
+            controller: WavesPageHeaderController,
+            bindings: {
+                title: '@',
+                subtitle: '<'
+            },
+            template: '<div class="page-header"><h1>{{$ctrl.title}}<small ng-if="$ctrl.subtitle.length > 0">' +
+                '<i class="glyphicon glyphicon-menu-right"></i>{{$ctrl.subtitle}}</small></h1></div>'
+        });
+})();
