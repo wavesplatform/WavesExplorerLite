024d9bcdfc7c34063d89614520058f1c4d1e159c
beregovoy68 <s.a.ishchenko@gmail.com>
2018-03-08 23:19:16 +1100

    Updated devnet nodes list

--- src/js/config.devnet.js
+++ src/js/config.devnet.js
@@ -7,8 +7,11 @@
         blockchainName: 'Devnet',
         nodes: [
             {url: 'http://34.251.200.245:6869', maintainer: 'Waves', showAsLink: true},
+            {url: 'http://34.237.49.199:6869', maintainer: 'Waves', showAsLink: true},
             {url: 'http://35.157.212.173:6869', maintainer: 'Waves', showAsLink: true},
-            {url: 'http://13.229.61.140:6869', maintainer: 'Waves', showAsLink: true}
+            {url: 'http://52.28.115.7:6869', maintainer: 'Waves', showAsLink: true},
+            {url: 'http://13.229.61.140:6869', maintainer: 'Waves', showAsLink: true},
+            {url: 'http://52.192.190.54:6869', maintainer: 'Waves', showAsLink: true}
         ],
         peerExplorer: {
             url: 'http://devnet.wavesexplorer.com',
