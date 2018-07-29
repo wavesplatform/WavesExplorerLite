73b0f46add1136035984543e8183c092dce14d50
beregovoy68 <s.a.ishchenko@gmail.com>
2018-07-30 00:21:37 +1000

    Added peers page.

--- src/peers/PeersPage.js
+++ src/peers/PeersPage.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
+import PeerList from './PeerList';
+
 export default class PeersPage extends React.Component {
     render() {
+        const peers = [{
+            address: '/52.8.224.179:43164',
+            declaredAddress: '/52.8.224.179:6868',
+            name: 'waves-official-eu-node-16',
+            nonce: 539056
+        }, {
+            address: '/52.8.224.179:43164',
+            declaredAddress: '/52.8.224.179:6868',
+            name: 'waves-official-eu-node-16',
+            nonce: 539056
+        }, {
+            address: '/52.8.224.179:43164',
+            declaredAddress: '/52.8.224.179:6868',
+            name: 'waves-official-eu-node-16',
+            nonce: 539056
+        }, {
+            address: '/52.8.224.179:43164',
+            declaredAddress: '/52.8.224.179:6868',
+            name: 'waves-official-eu-node-16',
+            nonce: 539056
+        }];
+
         return (
-            <div>Peers</div>
+            <React.Fragment>
+                <div className="headline">
+                    <span className="title">Peers</span>
+                    <label className="right">
+                        <span>Connected </span>
+                        <span className="bold">{peers.length}</span>
+                    </label>
+                </div>
+                <PeerList peers={peers} />
+            </React.Fragment>
         );
     }
 }
