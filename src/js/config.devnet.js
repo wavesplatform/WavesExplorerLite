a07d1bb7f61cc27f527abbc0e4b72ef3e510f094
beregovoy68 <s.a.ishchenko@gmail.com>
2018-10-02 23:00:52 +1000

    Updated IP address for a devnet node

--- src/js/config.devnet.js
+++ src/js/config.devnet.js
@@ -7,7 +7,7 @@
         title: 'Waves Devnet Explorer',
         blockchainName: 'Devnet',
         nodes: [
-            {url: 'http://34.251.200.245:6869', maintainer: 'Waves', showAsLink: true},
+            {url: 'http://18.203.15.139:6869', maintainer: 'Waves', showAsLink: true},
             {url: 'http://34.237.49.199:6869', maintainer: 'Waves', showAsLink: true},
             {url: 'http://35.157.212.173:6869', maintainer: 'Waves', showAsLink: true},
             {url: 'http://52.28.115.7:6869', maintainer: 'Waves', showAsLink: true},
