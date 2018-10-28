5166dc89eb00f764383fa1cfb75fcd552ada978f
beregovoy68 <s.a.ishchenko@gmail.com>
2018-10-28 20:43:41 +1100

    Updated devnet node address

--- src/js/config.devnet.js
+++ src/js/config.devnet.js
@@ -11,7 +11,7 @@
             {url: 'http://34.237.49.199:6869', maintainer: 'Waves', showAsLink: true},
             {url: 'http://35.157.212.173:6869', maintainer: 'Waves', showAsLink: true},
             {url: 'http://52.28.115.7:6869', maintainer: 'Waves', showAsLink: true},
-            {url: 'http://13.229.61.140:6869', maintainer: 'Waves', showAsLink: true},
+            {url: 'http://13.228.127.88:6869', maintainer: 'Waves', showAsLink: true},
             {url: 'http://52.192.190.54:6869', maintainer: 'Waves', showAsLink: true}
         ],
         peerExplorer: {
