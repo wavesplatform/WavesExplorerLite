c7f36f58f3434d717c2b3285becb140434ac9fcb
uiskander <theodsgn@gmail.com>
2018-12-19 16:29:03 +0300

    explorer-redesign: fix loader size

--- src/Fallback.js
+++ src/Fallback.js
@@ -4,12 +4,12 @@ import appLoading from './images/app-loading.gif';
 
 const Fallback = () => {
     return (
-        <img src={appLoading} height={250} width={250} alt="Loading..." style={{
+        <img src={appLoading} height={50} width={50} alt="Loading..." style={{
             position: 'absolute',
             top: '50%',
             left: '50%',
-            marginTop: '-125px', /* Half the height */
-            marginLeft: '-125px', /* Half the width */
+            marginTop: '-25px', /* Half the height */
+            marginLeft: '-25px', /* Half the width */
         }}/>
     );
 };
