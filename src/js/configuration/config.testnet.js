3d50d79da08ec44ecfee3384d20371432a7a1cf5
beregovoy68 <s.a.ishchenko@gmail.com>
2019-05-22 22:05:16 +1000

    EXP-237: Moved decompile script configuration to webpack. Now it's available regardless of selected network.

--- src/js/configuration/config.testnet.js
+++ src/js/configuration/config.testnet.js
@@ -5,7 +5,6 @@ export default {
     displayName: 'Testnet',
     apiBaseUrl: nodeUrl,
     dataServicesBaseUrl: 'https://api.testnet.wavesplatform.com/v0',
-    decompileScriptUrl: 'https://testnode1.wavesnodes.com/utils/script/decompile',
     nodes: [
         {url: nodeUrl, maintainer: 'Waves', showAsLink: true},
         {url: 'https://testnode2.wavesnodes.com', maintainer: 'Waves', showAsLink: true},
