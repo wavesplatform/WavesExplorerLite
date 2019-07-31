eba4d47da8b55e8e74c674d28977743a480460f2
beregovoy68 <s.a.ishchenko@gmail.com>
2019-08-01 00:05:43 +1000

    EXP-452: Fixed tooltip hide logic

--- src/js/components/Tooltip/Tooltip.view.js
+++ src/js/components/Tooltip/Tooltip.view.js
@@ -3,13 +3,12 @@ import PropTypes from 'prop-types';
 import ReactTooltip from 'react-tooltip';
 
 const HIDE_TOOLTIP_DELAY = 2000;
-const hideAllTooltips = () => ReactTooltip.hide();
 
 export const TooltipView = ({id}) => (
     <ReactTooltip id={id}
                   effect="solid"
                   className="tooltip"
-                  afterShow={() => setTimeout(hideAllTooltips, HIDE_TOOLTIP_DELAY)} />
+                  delayHide={HIDE_TOOLTIP_DELAY} />
 );
 
 TooltipView.propTypes = {
