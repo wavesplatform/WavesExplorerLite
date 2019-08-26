15c4d26c398db577b28fdeefb2dda1082c796419
beregovoy68 <s.a.ishchenko@gmail.com>
2019-08-26 22:18:54 +1000

    EXP-522: Updated stagenet configuration to support faucet

--- src/js/configuration/config.stagenet.js
+++ src/js/configuration/config.stagenet.js
@@ -6,5 +6,10 @@ export default {
     apiBaseUrl: nodeUrl,
     useCustomRequestConfig: true,
     dataServicesBaseUrl: 'https://api-stagenet.wavesplatform.com/v0',
-    nodes: [{url: nodeUrl, maintainer: 'Waves', showAsLink: true}]
+    nodes: [{url: nodeUrl, maintainer: 'Waves', showAsLink: true}],
+    faucet: {
+        url: 'http://faucet-stagenet.wavesplatform.com',
+        captchaKey: '6Lfir7MUAAAAAAWip4okLvtW8de3fvZaZbGVN-bn',
+        address: '3Mi63XiwniEj6mTC557pxdRDddtpj7fZMMw'
+    }
 };
