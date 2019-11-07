051125aac33dd7865e24a46ff772fed909939e65
username1565 <39200328+username1565@users.noreply.github.com>
2019-11-07 19:02:12 +0200

    Fix node URL for remote access.
    Fix the URL for the node on the opened host,
    to open old wavesblockexplorer from external IP or domain name.

--- src/js/config-1.1.8.js
+++ src/js/config-1.1.8.js
@@ -3,15 +3,17 @@
 
     angular.module('web').constant('appConfig', {
         //apiDomain: 'https://waves.guarda.co',
-        apiDomain: 'http://127.0.0.1:6869',	//local node
+        //apiDomain: 'http://127.0.0.1:6869',	//local node
+        apiDomain: location.protocol+'//'+location.hostname+':6869',		//node on the current host with current protocol
         
 		dataServiceBaseUrl: 'https://api.wavesplatform.com/v0',
         blockchainName: 'Mainnet',
         title: 'Waves Explorer',
         nodes: [{
             //url: 'https://waves.guarda.co',
-            url: 'http://127.0.0.1:6869',	//local node
-        
+            //url: 'http://127.0.0.1:6869',	//local node
+            url: location.protocol+'//'+location.hostname+':6869',			//node on the current host with current protocol
+            
 			maintainer: 'Waves'
         }],
         peerExplorer: {
