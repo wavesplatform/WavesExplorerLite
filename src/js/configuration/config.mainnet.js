25784ea052a4bfb213b4bf3328d58e30d116bbaa
bnagtega <bram.nagtegals@trendminer.com>
2020-07-08 17:54:03 +0200

    update urls, add firefox turtleshell urls

--- src/js/configuration/config.mainnet.js
+++ src/js/configuration/config.mainnet.js
@@ -39,8 +39,9 @@ export default {
         {name: '$ETHO', url: 'https://kgbconcepts.network/ether1totn.html', maintainer:'https://kgbconcepts.network/'},
     ],
     tools: [
-        {name: 'Online Wallet & DEX', url: 'https://client.turtlenetwork.eu/',maintainer: 'https://turtlenetwork.eu'},
+        {name: 'Online Wallet & DEX', url: 'https://wallet.turtlenetwork.eu/',maintainer: 'https://turtlenetwork.eu'},
         {name: 'TurtleShell Chrome Store', url: 'https://chrome.google.com/webstore/detail/turtleshell/billhkeiifjfbbkmmfpcpchikbajnfje?hl=nl',maintainer: 'https://turtlenetwork.eu'},
+        {name: 'TurtleShell Firefox', url: 'https://addons.mozilla.org/en-US/firefox/addon/turtleshell/',maintainer: 'https://turtlenetwork.eu'},
         {name: 'TurtleShell tool', url: 'https://turtleshell.turtlenetwork.eu/',maintainer: 'https://turtlenetwork.eu'},
         {name: 'Android Wallet', url: 'https://play.google.com/store/apps/details?id=com.tn.wallet&hl=nl',maintainer: 'https://turtlenetwork.eu'},
         {name: 'Statistics', url: 'http://statistics.turtlenetwork.eu/'}
