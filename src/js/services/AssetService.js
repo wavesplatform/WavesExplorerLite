13629d2ffca909367fda574b4b5752a1f49b512f
nazhmik <n.baymurzaev@gmail.com>
2020-09-17 19:51:33 +0300

    EXP-667: asset-details-cache

--- src/js/services/AssetService.js
+++ src/js/services/AssetService.js
@@ -5,38 +5,65 @@ import {ApiClientService} from './ApiClientService';
 import {VostokToWavesEnterprise} from '../shared/constants';
 
 export class AssetService extends ApiClientService {
-    constructor(configurationService, networkId) {
+    constructor(configurationService, assetCache, networkId) {
         super(configurationService, networkId);
+
+        this.promisesCache = {};
+        this.assetCache = assetCache;
     }
 
-    loadDetails = (assetId) => {
-        return this.getApi().assets.details(assetId, true).then(detailsResponse => {
-            const data = detailsResponse.data;
-            // TODO: remove when token is renamed
-            if (data.assetId === VostokToWavesEnterprise.id) {
-                data.name = VostokToWavesEnterprise.name;
-                data.description = VostokToWavesEnterprise.description;
-            }
+    put = asset => {
+        this.assetCache.put(asset);
+    };
 
-            const currency = Currency.fromIssueTransaction(data);
+    async loadDetails(assetId) {
+        return this.assetCache.get(assetId)
+            .then(asset => {
+                if (asset) {
+                    return asset;
+                }
 
-            return {
-                id: data.assetId,
-                issued: {
-                    height: data.issueHeight,
-                    timestamp: new DateTime(data.issueTimestamp)
-                },
-                issuer: data.issuer,
-                name: data.name,
-                description: data.description,
-                decimals: data.decimals,
-                reissuable: data.reissuable,
-                quantity: Money.fromCoins(data.quantity, currency),
-                scripted: data.scripted,
-                scriptDetails: data.scripted ? data.scriptDetails : null,
-                minSponsoredFee: data.minSponsoredAssetFee ? Money.fromCoins(data.minSponsoredAssetFee, currency) : null,
-                originTransactionId: data.originTransactionId
-            }
-        })
+                if (this.promisesCache[assetId]) {
+                    return this.promisesCache[assetId];
+                }
+
+                const promise = this.getApi().assets.details(assetId).then(detailsResponse => {
+                    const data = detailsResponse.data;
+
+                    // TODO: remove when token is renamed
+                    if (data.assetId === VostokToWavesEnterprise.id) {
+                        data.name = VostokToWavesEnterprise.name;
+                        data.description = VostokToWavesEnterprise.description;
+                    }
+
+                    const currency = Currency.fromIssueTransaction(data);
+
+                    const details = {
+                        id: data.assetId,
+                        issued: {
+                            height: data.issueHeight,
+                            timestamp: new DateTime(data.issueTimestamp)
+                        },
+                        issuer: data.issuer,
+                        name: data.name,
+                        description: data.description,
+                        decimals: data.decimals,
+                        reissuable: data.reissuable,
+                        quantity: Money.fromCoins(data.quantity, currency),
+                        scripted: data.scripted,
+                        scriptDetails: data.scripted ? data.scriptDetails : null,
+                        minSponsoredFee: data.minSponsoredAssetFee ? Money.fromCoins(data.minSponsoredAssetFee, currency) : null,
+                        originTransactionId: data.originTransactionId
+                    } 
+
+                    this.put(details);
+
+                    return details;
+                })
+
+                this.promisesCache[assetId] = promise;
+
+                return promise;
+            });
     }
 }
