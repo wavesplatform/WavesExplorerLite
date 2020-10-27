010b47df016ed69a06499431b3f8a4663917d4da
Daniil <lobanovdanil97@gmail.com>
2020-10-27 13:08:48 +0300

    stab

--- src/js/configuration/config.mainnet.js
+++ src/js/configuration/config.mainnet.js
@@ -5,7 +5,7 @@ export default {
     displayName: 'Mainnet',
     apiBaseUrl: nodeUrl,
     useCustomRequestConfig: true,
-    dataServicesBaseUrl: 'https://api.wavesplatform.com/v0',
+    dataServicesBaseUrl: 'https://mainnet-htz-hel1-1.wavesnodes.com/',
     spamListUrl: 'https://raw.githubusercontent.com/wavesplatform/waves-community/master/Scam%20tokens%20according%20to%20the%20opinion%20of%20Waves%20Community.csv',
     nodes: [{url: nodeUrl, maintainer: 'Waves'}]
 };
