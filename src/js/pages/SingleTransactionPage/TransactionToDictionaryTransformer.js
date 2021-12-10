fae756470ab4073305769aef763b28445eb8c0e6
Daniil <48389713+Veserion@users.noreply.github.com>
2021-12-10 14:27:16 +0300

    Update from master (#137)
    * state update
    
    * Fix leases  (#136)
    
    * 1
    
    * Payment -> Transfer(Payment)
    
    * fix leases in result
    
    * fix leasings
    
    * fix leasings
    
    * update ride-js

--- src/js/pages/SingleTransactionPage/TransactionToDictionaryTransformer.js
+++ src/js/pages/SingleTransactionPage/TransactionToDictionaryTransformer.js
@@ -227,7 +227,7 @@ const cancelLeaseTransactionItems = tx => {
             ...buildTransactionHeaderItems(tx),
             {
                 label: 'Lease tx id',
-                value: <TransactionRef txId={tx.leaseId}/>
+                value: <TransactionRef txId={tx.originTransactionId}/>
             },
             {
                 label: 'Lease info',
