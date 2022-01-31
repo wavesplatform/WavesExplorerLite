4834ccda88dfb6c834d602290e4d3c00b54fad81
veserion <lobanovdanil97@gmail.com>
2022-01-31 10:10:09 +0300

    search by block height added

--- src/js/services/SearchService.js
+++ src/js/services/SearchService.js
@@ -33,43 +33,51 @@ export class SearchService extends ApiClientService {
                 return routes.blocks.one(heightResponse.height);
             });
         })
-        .catch(() => {
-            return api.transactions.info(query).then(infoResponse => {
-                const event = this.createEvent(SearchResult.transaction);
-                this.analyticsService.sendEvent(event);
+            .catch(() => {
+                return api.transactions.info(query).then(infoResponse => {
+                    const event = this.createEvent(SearchResult.transaction);
+                    this.analyticsService.sendEvent(event);
 
-                return routes.transactions.one(infoResponse.id);
-            });
-        }).catch(() => {
-            return this.aliasService.loadAddress(query).then(address => {
-                const event = this.createEvent(SearchResult.alias);
-                this.analyticsService.sendEvent(event);
+                    return routes.transactions.one(infoResponse.id);
+                });
+            }).catch(() => {
+                return this.aliasService.loadAddress(query).then(address => {
+                    const event = this.createEvent(SearchResult.alias);
+                    this.analyticsService.sendEvent(event);
 
-                return routes.addresses.one(address);
-            });
-        }).catch(() => {
-            return api.assets.details(query).then(detail => {
-                const event = this.createEvent(SearchResult.asset);
-                this.analyticsService.sendEvent(event);
+                    return routes.addresses.one(address);
+                });
+            }).catch(() => {
+                return api.assets.details(query).then(detail => {
+                    const event = this.createEvent(SearchResult.asset);
+                    this.analyticsService.sendEvent(event);
 
-                return routes.assets.one(detail.assetId);
+                    return routes.assets.one(detail.assetId);
+                })
             })
-        })
-        .catch(() => {
-            return api.transactions.leaseInfo([query]).then(detail => {
-                const event = this.createEvent(SearchResult.lease);
+            .catch(() => {
+                return api.transactions.leaseInfo([query]).then(detail => {
+                    const event = this.createEvent(SearchResult.lease);
+                    this.analyticsService.sendEvent(event);
+
+                    const lease = detail[0];
+                    return routes.leases.one(lease.id);
+                })
+            }).catch(() => {
+                if (!isNaN(query)) {
+                    return api.blocks.at(query).then(heightResponse => {
+                        const event = this.createEvent(SearchResult.block);
+                        this.analyticsService.sendEvent(event);
+
+                        return routes.blocks.one(heightResponse.height);
+                    });
+                }
+            }).catch(e => {
+                const event = this.createEvent(SearchResult.unknown);
                 this.analyticsService.sendEvent(event);
 
-                const lease = detail[0];
-                return routes.leases.one(lease.id);
-            })
-        })
-       .catch(e => {
-            const event = this.createEvent(SearchResult.unknown);
-            this.analyticsService.sendEvent(event);
-
-            throw e;
-        });
+                throw e;
+            });
     };
 
     createEvent = searchResult => new EventBuilder().search().events().results(searchResult).build();
