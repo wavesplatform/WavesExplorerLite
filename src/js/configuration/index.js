6228918cd3b3c227990f4b35fb9ee703fc98607d
inazarov <inazarov@web3tech.ru>
2022-06-17 11:06:54 +0300

    config

--- src/js/configuration/index.js
+++ src/js/configuration/index.js
@@ -3,7 +3,7 @@ import testnet from './config.testnet';
 import devnet from './config.devnet';
 import stagenet from './config.stagenet';
 
-const configuredNetworks = [stagenet, testnet, mainnet, devnet]
+const configuredNetworks = [mainnet, stagenet, testnet, , devnet]
     .filter(item => __NETWORKS__.includes(item.networkId));
 
 export default configuredNetworks;
